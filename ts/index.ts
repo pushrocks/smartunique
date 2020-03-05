6a8fca75c918b14528df24c5d3b69eb84a8987da
Phil Kunz <gitlab@philkunz.com>
2020-03-05 20:41:15 +0000

    fix(core): update

--- ts/index.ts
+++ ts/index.ts
@@ -18,8 +18,8 @@ export const uuid5 = (customStringArg: string, namespaceArg = plugins.uuid.v5.DN
   return plugins.uuid.v5(customStringArg, namespaceArg);
 };
 
-export const uni = () => {
-  return 'unixxxxxxxxxxx'.replace(/[xy]/g, c => {
+export const uni = (prefix: string = 'uni') => {
+  return `${prefix}xxxxxxxxxxx`.replace(/[xy]/g, c => {
     const r = (Math.random() * 16) | 0;
     const v = c === 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
